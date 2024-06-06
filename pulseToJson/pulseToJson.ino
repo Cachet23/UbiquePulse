8b3b7ca79f305830cef8799b8a2df50ce4c0167d
Nils Baierl <n.baierl1@oth-aw.de>
2024-06-06 09:24:49 +0200

    .ino file now sends redRawData

--- pulseToJson/pulseToJson.ino
+++ pulseToJson/pulseToJson.ino
@@ -1,8 +1,11 @@
 #include <Wire.h>
+#include "MAX30100.h"
 #include "MAX30100_PulseOximeter.h"
 
 #define REPORTING_PERIOD_MS 1000
 
+// Define a MAX30100 sensor instance
+MAX30100 sensor;
 PulseOximeter pox;
 uint32_t tsLastReport = 0;
 
@@ -12,9 +15,25 @@ void onBeatDetected() {
 
 void setup() {
     Serial.begin(115200);
-    Serial.print("Initializing pulse oximeter... ");
+    Serial.print("Initializing MAX30100... ");
+
+    // Initialize the MAX30100 sensor
+    if (!sensor.begin()) {
+        Serial.println("FAILED");
+        for(;;);
+    } else {
+        Serial.println("SUCCESS");
+    }
+
+    // Set up MAX30100 parameters
+    sensor.setMode(MAX30100_MODE_SPO2_HR);
+    sensor.setLedsCurrent(MAX30100_LED_CURR_50MA, MAX30100_LED_CURR_27_1MA);
+    sensor.setLedsPulseWidth(MAX30100_SPC_PW_1600US_16BITS);
+    sensor.setSamplingRate(MAX30100_SAMPRATE_100HZ);
+    sensor.setHighresModeEnabled(true);
 
     // Initialize the PulseOximeter instance
+    Serial.print("Initializing pulse oximeter... ");
     if (!pox.begin()) {
         Serial.println("FAILED");
         for(;;);
@@ -22,25 +41,29 @@ void setup() {
         Serial.println("SUCCESS");
     }
 
-    // The default current for the IR LED is 50mA and it could be changed
-    // by uncommenting the following line. Check MAX30100_PulseOximeter.h for all the available options.
-    // pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
-
-    // Register a callback for the beat detection
+    // Register a callback for beat detection
     pox.setOnBeatDetectedCallback(onBeatDetected);
 }
 
 void loop() {
-    // Make sure to call update as fast as possible
+    uint16_t ir, red;
+
+    // Update the sensor
+    sensor.update();
     pox.update();
 
-    // Grab the updated heart rate and SpO2
+    // Check if it's time to report
     if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
-        // Create JSON string with sensor data
-        String jsonData = "{ \"heartRate\": " + String(pox.getHeartRate()) + ", \"spO2\": " + String(pox.getSpO2()) + " }";
-        
-        // Send JSON string over serial
-        Serial.println(jsonData); // This adds a newline character automatically
+        // Get raw values
+        while (sensor.getRawValues(&ir, &red)) {
+            // Create JSON string with sensor data
+            String jsonData = "{ \"heartRate\": " + String(pox.getHeartRate()) + 
+                              ", \"spO2\": " + String(pox.getSpO2()) + 
+                              ", \"red\": " + String(red) + " }";
+            
+            // Send JSON string over serial
+            Serial.println(jsonData); // This adds a newline character automatically
+        }
 
         tsLastReport = millis();
     }
